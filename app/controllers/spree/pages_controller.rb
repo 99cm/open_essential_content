6d0ad78f0e54a7184d252e2ac24d7b90b0764285
99cm <zyizyao@china-guide.com>
2018-06-07 12:17:39 -0400

    Update pages_controller.rb

--- app/controllers/spree/pages_controller.rb
+++ app/controllers/spree/pages_controller.rb
@@ -1,20 +1,21 @@
-class Spree::PagesController < Spree::StoreController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+module Spree
+  class Spree::PagesController < Spree::StoreController
 
-  helper 'spree/blogs/posts'
-  helper 'spree/products'
+    helper 'spree/blogs/posts'
+    helper 'spree/products'
 
-  def show
-    @page = current_page
-    raise ActionController::RoutingError.new("No route matches [GET] #{request.path}") if @page.nil?
-    if @page.root?
-      @posts = Spree::Post.live.limit(5)
-      render template: 'spree/pages/home'
+    def show
+      @page = current_page
+      raise ActionController::RoutingError.new("No route matches [GET] #{request.path}") if @page.nil?
+      if @page.root?
+        @posts = Spree::Post.live.limit(5)
+        render template: 'spree/pages/home'
+      end
     end
+
+    private
+      def accurate_title
+        @page.meta_title
+      end
   end
-
-  private
-    def accurate_title
-      @page.meta_title
-    end
 end
