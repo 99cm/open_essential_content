c49be36dacb15a0c49df11cae9af7e2de01e5027
99cm <zyizyao@china-guide.com>
2018-06-07 11:54:26 -0400

    fix rubocop errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
-Spree::Core::Engine.append_routes do
+Spree::Core::Engine.add_routes do
   namespace :admin do
     resources :uploads
 
-    resources :pages, :constraints => { :id => /.*/ } do
+    resources :pages, constraints: { id: /.*/ } do
       collection do
         post :update_positions
       end
@@ -13,27 +13,27 @@ Spree::Core::Engine.append_routes do
         end
       end
 
-      resources :images, :controller => "page_images" do
+      resources :images, controller: 'page_images' do
         collection do
           post :update_positions
         end
       end
     end
 
-    resources :blogs, :constraints => { :id => /[a-z0-9\-\_\/]{3,}/ }
+    resources :blogs, constraints: { id: /[a-z0-9\-\_\/]{3,}/ }
 
     resources :posts, except: [:show] do
-      resources :images, except: [:show], controller: "post_images" do
+      resources :images, except: [:show], controller: 'post_images' do
         collection do
           post :update_positions
         end
       end
-      resources :products, execpt: [:show, :edit], controller: "post_products" do
+      resources :products, execpt: [:show, :edit], controller: 'post_products' do
         collection do
           post :update_positions
         end
       end
-      resources :categories, except: [:show], :controller => "post_categories"
+      resources :categories, except: [:show], controller: 'post_categories'
     end
 
     resource :content_settings, only: [:edit, :update]
@@ -41,18 +41,18 @@ Spree::Core::Engine.append_routes do
 
   constraints(Spree::PossibleBlog) do
     constraints(
-      :year  => /\d{4}/,
-      :month => /\d{1,2}/,
-      :day   => /\d{1,2}/
+      year: /\d{4}/,
+      month: /\d{1,2}/,
+      day: /\d{1,2}/
     ) do
-      get ":blog_id/:year(/:month(/:day))" => "posts#index", :as => :post_date
-      get ":blog_id/:year/:month/:day/:id" => "posts#show",  :as => :full_post
+      get ':blog_id/:year(/:month(/:day))' to: 'posts#index', as: :post_date
+      get ':blog_id/:year/:month/:day/:id' to: 'posts#show',  as: :full_post
     end
 
-    get ":blog_id/category/:id"   => "post_categories#show", :as => :post_category, :constraints => { :id => /.*/ }
-    get ":blog_id/search/:query"  => "posts#search",         :as => :search_posts, :query => /.*/
-    get ":blog_id/archive"        => "posts#archive",        :as => :archive_posts
-    get ":blog_id"                => "posts#index",          :as => :blog_posts
+    get ':blog_id/category/:id' to: 'post_categories#show', as: :post_category, constraints: { id: /.*/ }
+    get ':blog_id/search/:query' to: 'posts#search', as: :search_posts, query: /.*/
+    get ':blog_id/archive' to: 'posts#archive', as: :archive_posts
+    get ':blog_id' to: 'posts#index', as: :blog_posts
   end
 
   constraints(Spree::PossiblePage) do
