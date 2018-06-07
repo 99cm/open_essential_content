d91ce296c10bf92ce7c2fd414c5f25b9f30c6579
99cm <zyizyao@china-guide.com>
2018-06-07 13:43:18 -0400

    fix rubocop errors

--- app/models/spree/post_image.rb
+++ app/models/spree/post_image.rb
@@ -5,7 +5,7 @@ class Spree::PostImage < Spree::Asset
     url: '/spree/posts/:id/:style/:basename.:extension',
     path: ':rails_root/public/spree/posts/:id/:style/:basename.:extension'
 
-  validates_attachment_content_type :attachment, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_attachment_content_type :attachment, content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
   validates_attachment_presence :attachment
 
   def image_content?
