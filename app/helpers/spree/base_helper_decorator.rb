1dc8532ded46ce4f33d0915c653ae44c849ce094
99cm <zyizyao@china-guide.com>
2018-06-07 12:57:46 -0400

    Update base_helper_decorator.rb

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,7 +1,7 @@
 module Spree
   module BaseHelper
     def content_snippets
-      Spree::Page.where(title: "Content Snippets").first
+      Spree::Page.where(title: 'Content Snippets').first
     end
   end
 end
