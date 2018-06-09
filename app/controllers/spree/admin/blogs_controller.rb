3fc9a46299729e06e941f939376eac0c28e97a01
99cm <zyizyao@china-guide.com>
2018-06-09 12:05:18 -0400

    Add # frozen_string_literal: true

--- app/controllers/spree/admin/blogs_controller.rb
+++ app/controllers/spree/admin/blogs_controller.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 module Spree
   module Admin
     class BlogsController < ResourceController
-
       def show
         redirect_to admin_blogs_path
       end
@@ -14,7 +15,7 @@ module Spree
 
       def collection
         params[:search] ||= {}
-        params[:search][:meta_sort] ||= "name.asc"
+        params[:search][:meta_sort] ||= 'name.asc'
         @search = Spree::Blog.search(params[:q])
         @collection = @search.result.page(params[:page]).per(Spree::Config[:admin_orders_per_page])
       end
