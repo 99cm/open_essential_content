b6f708dcae2e8f0ee8d9b4bb4125209762e25198
99cm <zyizyao@china-guide.com>
2018-06-09 13:33:08 -0400

    Add # frozen_string_literal: true

--- app/controllers/spree/admin/uploads_controller.rb
+++ app/controllers/spree/admin/uploads_controller.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
 module Spree
   module Admin
     class UploadsController < ResourceController
       private
 
-        def find_resource
-          @upload ||= Spree::Upload.find_by_id(params[:id])
-        end
+      def find_resource
+        @upload ||= Spree::Upload.find_by_id(params[:id])
+      end
 
-        def collection
-          return @collection if @collection.present?
-          params[:q] ||= {}
-          params[:q][:s] ||= 'attachment_updated_at desc'
-          @search = Spree::Upload.search(params[:q])
-          @collection = @search.result.page(params[:page]).per(Spree::Config[:admin_orders_per_page])
-        end
+      def collection
+        return @collection if @collection.present?
+        params[:q] ||= {}
+        params[:q][:s] ||= 'attachment_updated_at desc'
+        @search = Spree::Upload.search(params[:q])
+        @collection = @search.result.page(params[:page]).per(Spree::Config[:admin_orders_per_page])
+      end
     end
   end
 end
