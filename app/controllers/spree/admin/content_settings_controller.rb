c1b71e0704f9274bbd883ef54845bd3b02fc9beb
99cm <zyizyao@china-guide.com>
2018-06-09 12:10:24 -0400

    Add # frozen_string_literal: true

--- app/controllers/spree/admin/content_settings_controller.rb
+++ app/controllers/spree/admin/content_settings_controller.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 module Spree
   module Admin
     class ContentSettingsController < BaseController
       def edit
-        @preferences = [
-          'disqus_shortname', 'sharethis_publisher_id', 'show_posts_on_homepage',
-          'show_products_on_homepage', 'show_taxons_on_homepage'
+        @preferences = %w[
+          disqus_shortname sharethis_publisher_id show_posts_on_homepage
+          show_products_on_homepage show_taxons_on_homepage
         ]
         @config = Spree::ContentConfiguration.new
       end
@@ -20,7 +22,6 @@ module Spree
         flash[:success] = t('spree.successfully_updated', resource: t('spree_essential_content.admin.content_settings'))
         redirect_to edit_admin_content_settings_path
       end
-
     end
   end
 end
