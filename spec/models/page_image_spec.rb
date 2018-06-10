b24616663803d00e604702dd4e29a7adcac3ecb3
99cm <zyizyao@china-guide.com>
2018-06-10 12:26:13 -0400

    Update page_image_spec.rb

--- spec/models/page_image_spec.rb
+++ spec/models/page_image_spec.rb
@@ -1,13 +1,11 @@
 require 'spec_helper'
 
-module Spree
-  describe Spree::PageImage, type: :model do
-    it "is valid with a file" do
-      expect(build(:page_image)).to be_valid
-    end
+RSpec.describe Spree::PageImage, type: :model do
+  it 'is valid with a file' do
+    expect(build(:page_image)).to be_valid
+  end
 
-    it "is not valid when no file is attached" do
-      expect(build(:page_image, attachment: nil)).to_not be_valid
-    end
+  it 'is not valid when no file is attached' do
+    expect(build(:page_image, attachment: nil)).to_not be_valid
   end
 end
