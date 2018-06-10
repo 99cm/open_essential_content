6c54128db4daf88a891ae79998c76336f5df6207
99cm <zyizyao@china-guide.com>
2018-06-10 00:25:52 -0400

    Update to migrations to rails 5.2

--- db/migrate/20120306185750_acts_as_taggable_on_posts.rb
+++ db/migrate/20120306185750_acts_as_taggable_on_posts.rb
@@ -1,4 +1,4 @@
-class ActsAsTaggableOnPosts < ActiveRecord::Migration[4.2]
+class ActsAsTaggableOnPosts < ActiveRecord::Migration[5.2]
   def self.up
     return if table_exists? :tags
 
@@ -11,12 +11,12 @@ class ActsAsTaggableOnPosts < ActiveRecord::Migration[4.2]
 
       # You should make sure that the column created is
       # long enough to store the required class names.
-      t.references :taggable, :polymorphic => true
-      t.references :tagger, :polymorphic => true
+      t.references :taggable, polymorphic: true
+      t.references :tagger, polymorphic: true
 
       # Limit is created to prevent MySQL error on index
       # length for MyISAM table type: http://bit.ly/vgW2Ql
-      t.string :context, :limit => 128
+      t.string :context, limit: 128
 
       t.datetime :created_at
     end
