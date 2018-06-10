2004eede0a94ea5aed655ef6b8f251e8b8bee47d
99cm <zyizyao@china-guide.com>
2018-06-10 01:09:30 -0400

    Add globalize

--- lib/spree_essential_content.rb
+++ lib/spree_essential_content.rb
@@ -1,11 +1,12 @@
 require 'spree_core'
-require "acts-as-taggable-on"
+require 'acts-as-taggable-on'
 
 require 'spree_essential_content/engine'
 
 module Spree
   class PossiblePage
     def self.matches?(request)
+      path = request.path.gsub(/#{SpreeI18n::Config.available_locales.join('|')}\//, '').sub(%r{^/}, '')
       return false if request.path =~ Spree::Page::RESERVED_PATHS
       Spree::Page.active.where(path: Spree::Page.normalize_path(request.path)).any?
     end
@@ -15,7 +16,8 @@ module Spree
     def self.matches?(request)
       return false if request.path =~ Spree::Blog::RESERVED_PATHS
       path = request.path
-      path = path.sub(%r{^/},"").split(%r{/\s*})[0] unless path == "/"
+      path = path.sub(%r{^/},'').split(%r{/\s*}).reject {|p| SpreeI18n::Config.available_locales.include? p.to_sym}
+      path = path[0].tap{|s| s.slice!('.rss')} unless path == '/'
       Spree::Blog.where(permalink: Spree::Blog.normalize_permalink(path)).any?
     end
   end
